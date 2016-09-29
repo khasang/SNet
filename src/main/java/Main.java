b58de64b935011b8ad0a150f96590169584cbb4a
azon <azon.sk@gmail.com>
2016-09-29 22:01:30 +0400

    change sout

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("asda");
-        System.out.println("as231a");
-        System.out.println("asd12313");
+        System.out.println("as23adad1a");
+        System.out.println("asd12asdad313");
     }
 }
