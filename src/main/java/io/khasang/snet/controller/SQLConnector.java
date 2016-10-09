d0f55c0c85f6c8d98ef7bc69c8d9aeee5b052173
notsure <ASir2089@gmail.com>
2016-10-09 20:14:52 +0300

    Created class will use to connect with Data Base.

--- src/main/java/io/khasang/snet/controller/SQLConnector.java
+++ src/main/java/io/khasang/snet/controller/SQLConnector.java
@@ -1,8 +1,22 @@
 package io.khasang.snet.controller;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
-import java.sql.Connection;
+import javax.sql.DataSource;
 
 public class SQLConnector {
-    Connection connection = 
+    private final static String url = "jdbc:postgresql://localhost:5432/test_database?user=root&password=root";
+    private static JdbcTemplate jdbcTemplateInstance;
+
+    private static DataSource getDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(url);
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        return dataSource;
+    }
+
+    public static JdbcTemplate getJdbcTemplate() {
+        if (jdbcTemplateInstance==null) jdbcTemplateInstance = new JdbcTemplate(getDataSource());
+        return jdbcTemplateInstance;
+    }
 }
