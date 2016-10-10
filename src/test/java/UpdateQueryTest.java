9e79cb7f85b5cc44ee98a3fe16caa1e8356b61ee
notsure <ASir2089@gmail.com>
2016-10-10 12:11:13 +0300

    Set test class executable, also commit was added

--- src/test/java/UpdateQueryTest.java
+++ src/test/java/UpdateQueryTest.java
@@ -1,14 +1,24 @@
-import io.khasang.snet.controller.SQLQueryHandler;
+import io.khasang.snet.config.AppConfig;
+import io.khasang.snet.controller.QueryHandler;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.sql.PreparedStatement;
 
-/* Test for update query, specific for table
-*  describing by model.WeatherReport
+/* Test for update query, specific for test table weather columns
+* city, temp_lo, temp_hi, prcp, date
 */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = AppConfig.class)
 public class UpdateQueryTest {
+
+    @Autowired
+    QueryHandler queryHandler;
 
     /* Failed if rowAffected equals -1
      * this value tell that some error
@@ -16,11 +26,14 @@ public class UpdateQueryTest {
      */
     @Test
     public void queryUpdateTest() {
-        Integer rowAffected =
-                SQLQueryHandler.getInstance().executeUpdate(preparedStatementCreate(7,11));
+        Integer rowAffected = queryHandler.executeUpdate(preparedStatementCreate(7,11));
         Assert.assertNotEquals("Error while quering.",new Integer(-1),rowAffected);
     }
 
+    /* Creation PreparedStatementCreator implementation
+    *  @param updating value
+    *  @param key value
+    *  */
     private PreparedStatementCreator preparedStatementCreate(Integer value, Integer key) {
         return connection -> {
             PreparedStatement statement =
