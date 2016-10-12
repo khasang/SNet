721ed9ac1e8765b24afae32486d2f9aa38efe5f5
notsure <ASir2089@gmail.com>
2016-10-12 19:57:53 +0300

    add spring security with in memory auth

---
+++ src/main/java/io/khasang/snet/config/SecurityInit.java
@@ -0,0 +1,6 @@
+package io.khasang.snet.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
