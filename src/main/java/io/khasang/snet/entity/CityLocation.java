be9ed64a72f5e093b9db39012c8997e9cebbf9c2
notsure <ASir2089@gmail.com>
2016-10-16 17:12:45 +0300

    Class body was added

--- src/main/java/io/khasang/snet/entity/CityLocation.java
+++ src/main/java/io/khasang/snet/entity/CityLocation.java
@@ -1,7 +1,43 @@
 package io.khasang.snet.entity;
 
-/**
- * Created by cresh on 16.10.16.
- */
+import javax.persistence.*;
+
+@Entity
 public class CityLocation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "city")
+    private String cityName;
+
+    @Column(name = "location")
+    private int[] location;
+
+    public CityLocation() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int[] getLocation() {
+        return location;
+    }
+
+    public void setLocation(int[] location) {
+        this.location = location;
+    }
 }
