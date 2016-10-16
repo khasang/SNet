a03bc12ea25d806118975a45e6c64c2f41a1ecff
Konstantin <konstpavlovsdr@gmail.com>
2016-10-16 16:50:22 +0300

    fix hibernate.properties

--- src/main/java/io/khasang/snet/config/HibernateConfig.java
+++ src/main/java/io/khasang/snet/config/HibernateConfig.java
@@ -51,6 +51,7 @@ public class HibernateConfig {
         properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
         properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
         properties.put("hibernate.hbm2ddl.auto", environment.getRequiredProperty("hibernate.hbm2ddl.auto"));
+        properties.put("hibernate.temp.use_jdbc_metadata_defaults", false);
         return properties;
     }
 
