e174d437feb43d878542d435cfc0e172c28c0dde
notsure <ASir2089@gmail.com>
2016-10-21 21:53:24 +0300

    Tests for PicturesDataUtils was created

--- src/test/java/io/khasang/snet/util/PictureDataUtilTest.java
+++ src/test/java/io/khasang/snet/util/PictureDataUtilTest.java
@@ -7,6 +7,8 @@ import io.khasang.snet.dao.HibernateDAO;
 import io.khasang.snet.dao.impl.PicturesDataUtil;
 import io.khasang.snet.entity.Picture;
 import static org.junit.Assert.*;
+
+import io.khasang.snet.util.common.PicturesGenerator;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,6 +16,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
@@ -42,5 +48,45 @@ public class PictureDataUtilTest {
 
         Picture deSerialized = picturesDataUtil.get(original.getId());
         assertEquals(original,deSerialized);
+    }
+
+    @Test
+    public void updateTest() {
+        Picture original = generator.create();
+        picturesDataUtil.add(original);
+
+        Picture differed = generator.create();
+        differed.setId(original.getId());
+        differed.setDescription(original.getDescription());
+        differed.setOwnerId(original.getOwnerId());
+        assertNotEquals("Failed: original and differed haven't difference", original, differed);
+
+        picturesDataUtil.edit(differed);
+        Picture deSerialized = picturesDataUtil.get(differed.getId());
+        assertEquals("Failed: objects differs after serialization", differed, deSerialized);
+        assertNotEquals("Failed original and edited object haven't defference", original, deSerialized);
+    }
+
+    @Test
+    public void deleteTest() {
+        Picture picture = generator.create();
+        picturesDataUtil.add(picture);
+
+        picturesDataUtil.delete(picture.getId());
+        Picture deleted = picturesDataUtil.get(picture.getId());
+        assertNull(deleted);
+    }
+
+    @Test
+    public void listReturnTest() {
+        Set<Picture> pictureSet = new HashSet<>();
+        for (Picture picture : new PicturesGenerator(10)) {
+            picturesDataUtil.add(picture);
+            pictureSet.add(picture);
+        }
+
+        List<Picture> pictures = picturesDataUtil.getAll();
+        pictureSet.removeAll(pictures);
+        assertEquals(0,pictureSet.size());
     }
 }
