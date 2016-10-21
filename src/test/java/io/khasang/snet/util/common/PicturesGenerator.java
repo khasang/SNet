097ed21d2e5c63d1493e3766daff2aabca3c79cb
notsure <ASir2089@gmail.com>
2016-10-21 17:38:32 +0300

    Random generator of pictures added.

--- src/test/java/io/khasang/snet/util/common/PicturesGenerator.java
+++ src/test/java/io/khasang/snet/util/common/PicturesGenerator.java
@@ -1,7 +1,56 @@
 package io.khasang.snet.util.common;
 
+import io.khasang.snet.entity.Picture;
+import io.khasang.snet.util.Generator;
+
+import java.util.Iterator;
+import java.util.Random;
+
 /**
- * Created by cresh on 21.10.16.
+ * Generate random Picture objects
  */
-public class PicturesGenerator {
+public class PicturesGenerator implements Generator<Picture>, Iterable<Picture> {
+
+    private final String desc = "Random generated picture entity instance.";
+    private int amountOfPictures;
+    private Random random;
+
+    public PicturesGenerator() {
+        this.random = new Random(42);
+    }
+
+    public PicturesGenerator(int amountOfPictures) {
+        this.random = new Random(42);
+        this.amountOfPictures = amountOfPictures;
+    }
+
+    @Override
+    public Picture create() {
+        return new Picture(desc,getRandomGeneratedByteArray(random.nextInt(10240)),random.nextLong());
+    }
+
+    @Override
+    public Iterator<Picture> iterator() {
+        return new Iterator<Picture>() {
+            private int index = 0;
+            @Override
+            public boolean hasNext() {
+                return index < amountOfPictures;
+            }
+
+            @Override
+            public Picture next() {
+                return PicturesGenerator.this.create();
+            }
+        };
+    }
+
+    private byte[] getRandomGeneratedByteArray(int length) {
+        byte[] result = new byte[length];
+        for (int i = 0; i < length; i++) {
+            result[i] = (byte) random.nextInt(255);
+        }
+        return result;
+    }
+
 }
