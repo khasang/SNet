f85a94046319347d38b9c0a14bf6ec92c30ec4e4
notsure <ASir2089@gmail.com>
2016-11-02 18:31:47 +0300

    Added class for work with messages and json raw them representation

---
+++ src/main/java/io/khasang/snet/service/MessageTokenizer.java
@@ -0,0 +1,59 @@
+package io.khasang.snet.service;
+
+
+import io.khasang.snet.dao.AbstractCRUD;
+import io.khasang.snet.entity.Chat;
+import io.khasang.snet.entity.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageTokenizer {
+
+    @Autowired
+    private JsonSerializer<Message> messageSerializer;
+
+    @Autowired
+    private AbstractCRUD<Message> dataUtilMessages;
+
+    public String getList(Chat chat) {
+        Message message = new Message();
+        message.setChat(chat);
+
+        try {
+            return messageSerializer.parseToJson(dataUtilMessages.getAll(message));
+        } catch (Exception exc) {
+            return String.format("Error occur while quering list of messages. %s",exc.getMessage());
+        }
+    }
+
+    public String addNew(String raw) {
+        try {
+            Message message = messageSerializer.parseToEntity(raw,Message.class);
+            dataUtilMessages.add(message);
+        } catch (Exception exc) {
+            return String.format("Error occur while saving message: %s", exc.getMessage());
+        }
+        return "";
+    }
+
+    public String getOne(String raw) {
+        try {
+            Message message = messageSerializer.parseToEntity(raw, Message.class);
+            message = dataUtilMessages.get(message);
+            return messageSerializer.parseToJson(message);
+        } catch (Exception exc) {
+            return String.format("Error occur while getting existed message: %s", exc.getMessage());
+        }
+    }
+
+    public String delete(String raw) {
+        try {
+            Message message = messageSerializer.parseToEntity(raw, Message.class);
+            dataUtilMessages.delete(message);
+        } catch (Exception exc) {
+            return String.format("Error occur while deleting message: %s", exc.getMessage());
+        }
+        return "";
+    }
+}
