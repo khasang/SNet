ce414b21c49594090417af43cae971e92334fdc8
notsure <ASir2089@gmail.com>
2016-11-02 18:39:24 +0300

    Method for creation prime entry to application was added. Also annotation Rollback to few methods added

--- src/test/java/io/khasang/snet/entity/RegistryTest.java
+++ src/test/java/io/khasang/snet/entity/RegistryTest.java
@@ -5,6 +5,10 @@ import io.khasang.snet.config.HibernateConfig;
 import io.khasang.snet.config.application.WebConfig;
 import io.khasang.snet.dao.AbstractCRUD;
 import io.khasang.snet.dao.AbstractRegistrySearcher;
+import io.khasang.snet.dao.userauth.AuthRulesDAO;
+import io.khasang.snet.dao.userauth.RolesDAO;
+import io.khasang.snet.entity.userauth.AuthRules;
+import io.khasang.snet.entity.userauth.Roles;
 import io.khasang.snet.entity.userauth.User;
 import io.khasang.snet.util.Generator;
 import org.junit.Before;
@@ -12,9 +16,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import static org.junit.Assert.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -37,6 +44,12 @@ public class RegistryTest {
     @Autowired
     private Generator<Chat> chatGenerator;
 
+    @Autowired
+    private AuthRulesDAO authRulesDAO;
+
+    @Autowired
+    private RolesDAO rolesDAO;
+
     private Chat chat;
     private User user;
 
@@ -51,9 +64,36 @@ public class RegistryTest {
             userCRUD.add(user);
         }
     }
+    /* Creation ADMIN user account */
+    @Test
+    public void createPrimeEntry() {
+        String login = "admin";
+        String passwd = "admin";
+        String primeRole = "ROLE_ADMIN";
+
+        /* Creation admin account */
+        User admin = new User();
+        admin.setLogin(login);
+        admin.setPassword(new BCryptPasswordEncoder().encode(passwd));
+        userCRUD.add(admin);
+
+        /* Creation admin role */
+        Roles prime = new Roles();
+        prime.setRole(primeRole);
+        rolesDAO.addRoles(prime);
+
+        /* Creation of admin authorisation rule */
+        AuthRules rule = new AuthRules();
+        rule.setUser_id(admin.getID());
+        rule.setRole_id(prime.getId());
+        authRulesDAO.addAuthRules(rule);
+
+    }
 
 
     @Test
+    @Rollback
+    @Transactional
     public void saveTest() {
         ChatRegistryUnit original = new ChatRegistryUnit();
         original.setChat(chat);
@@ -70,6 +110,8 @@ public class RegistryTest {
 
 
     @Test
+    @Rollback
+    @Transactional
     public void gettingListTest() {
         User anotherUser = new User();
         userCRUD.add(anotherUser);
@@ -82,8 +124,8 @@ public class RegistryTest {
             chats.add(c);
         }
 
-        for (int i = 0; i < chats.size(); i++) {
-            registryCRUD.add(new ChatRegistryUnit(chats.get(i), anotherUser));
+        for (Chat chat1 : chats) {
+            registryCRUD.add(new ChatRegistryUnit(chat1, anotherUser));
         }
 
         Collection<Chat> firstUsersChat = chatCRUD.getListSearched(user);
