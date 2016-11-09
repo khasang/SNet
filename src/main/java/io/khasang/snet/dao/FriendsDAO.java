6f4430c3a16b8214e484546d73fc134c4001fd21
Konstantin <konstpavlovsdr@gmail.com>
2016-11-09 21:43:07 +0300

    add Friends DAO and Impl

---
+++ src/main/java/io/khasang/snet/dao/FriendsDAO.java
@@ -0,0 +1,19 @@
+package io.khasang.snet.dao;
+
+import io.khasang.snet.entity.Friends;
+import io.khasang.snet.entity.userauth.User;
+
+import java.util.List;
+
+public interface FriendsDAO {
+    // adding Invite to friends (approve = false)
+    void saveFriend(Friends friend);
+    // set approve to true in invite
+    void updateFriend(Friends friend);
+    List<Friends> getFriendsList(long id);
+    // get all friends from list where approve - false
+    List<Friends> getInviteList(long id);
+    // get line from database
+    Friends getFriendLine(long id);
+    Friends getInviteLine(long user,long friend);
+}
---
+++ src/main/java/io/khasang/snet/service/FriendsService.java
@@ -0,0 +1,84 @@
+package io.khasang.snet.service;
+
+
+import io.khasang.snet.dao.FriendsDAO;
+import io.khasang.snet.dao.profile.ProfileDAO;
+import io.khasang.snet.dao.userauth.UserDAO;
+import io.khasang.snet.entity.Friends;
+import io.khasang.snet.entity.profile.Profile;
+import io.khasang.snet.entity.userauth.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Transactional
+public class FriendsService {
+
+    @Autowired
+    FriendsDAO friendsDAO;
+
+    @Autowired
+    UserDAO userDAO;
+
+    @Autowired
+    ProfileDAO profileDAO;
+
+    // add invite to friends
+    public void addInvite(String login, String friend){
+        Friends friends = new Friends();
+        friends.setIdInUsers(userDAO.getUserByName(login).getID());
+        friends.setIdInFriends(userDAO.getUserByName(friend).getID());
+        friendsDAO.saveFriend(friends);
+    }
+
+    public void approveInvite(String login, String friend){
+        Friends friendLine= friendsDAO.getInviteLine(userDAO.getUserByName(friend).getID(),userDAO.getUserByName(login).getID());
+        friendLine.setApproved(true);
+        friendsDAO.updateFriend(friendLine);
+        // for friend double
+        Friends newFriend = new Friends();
+        newFriend.setIdInUsers(friendLine.getIdInFriends());
+        newFriend.setIdInFriends(friendLine.getIdInUsers());
+        newFriend.setApproved(true);
+        friendsDAO.saveFriend(newFriend);
+    }
+
+    public List<Profile> getFriendsList(String login){
+        User currentUser=userDAO.getUserByName(login);
+        List<Friends> friendsList = friendsDAO.getFriendsList(currentUser.getID());
+        List<User> userFriendsList= new ArrayList<>();
+
+        for (Friends fr: friendsList) {
+            userFriendsList.add(userDAO.getUserById((int) fr.getIdInFriends()));
+        }
+
+        List<Profile> profileFriendsList = new ArrayList<>();
+
+        for (User us: userFriendsList) {
+            profileFriendsList.add(profileDAO.getProfileByUserLogin(us.getLogin()));
+        }
+        return profileFriendsList;
+    }
+
+    public List<Profile> getInviteList(String login){
+        User currentUser=userDAO.getUserByName(login);
+        List<Friends> friendsList = friendsDAO.getInviteList(currentUser.getID());
+        List<User> userFriendsList= new ArrayList<>();
+
+        for (Friends fr: friendsList) {
+            userFriendsList.add(userDAO.getUserById((int) fr.getIdInUsers()));
+        }
+
+        List<Profile> profileFriendsList = new ArrayList<>();
+
+        for (User us: userFriendsList) {
+            profileFriendsList.add(profileDAO.getProfileByUserLogin(us.getLogin()));
+        }
+        return profileFriendsList;
+    }
+
+}
