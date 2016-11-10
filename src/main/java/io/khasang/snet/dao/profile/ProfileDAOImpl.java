9107360cdc3862dc790563a071abc6e9a1b614c7
Sergey <apay1234@mail.ru>
2016-11-10 17:09:12 +0300

    fix bug with incorrect behavior while updating both: user photos and data

--- src/main/java/io/khasang/snet/dao/profile/ProfileDAOImpl.java
+++ src/main/java/io/khasang/snet/dao/profile/ProfileDAOImpl.java
@@ -2,6 +2,8 @@ package io.khasang.snet.dao.profile;
 
 import io.khasang.snet.entity.profile.Profile;
 import org.hibernate.Criteria;
+import org.hibernate.Query;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,8 +28,28 @@ public class ProfileDAOImpl implements ProfileDAO {
 
     @Override
     public void updateProfile(Profile profile) {
-        sessionFactory.getCurrentSession().update(profile);
+        Session session =  sessionFactory.getCurrentSession();
+        Query query = session.createQuery(" UPDATE io.khasang.snet.entity.profile.Profile SET aboutMe = :aboutMe," +
+                "city = :city, interests = :interests, name = :name," +
+                "surname = :surname WHERE login = :userLogin");
+        query.setParameter("aboutMe", profile.getAboutMe());
+        query.setParameter("city", profile.getCity());
+        query.setParameter("interests", profile.getInterests());
+        query.setParameter("name", profile.getName());
+        query.setParameter("surname", profile.getSurname());
+        query.setParameter("userLogin", profile.getLogin());
+        int result = query.executeUpdate();
 
+    }
+
+    @Override
+    public void updateAvatar(String avatarName, String userLogin) {
+        Session session =  sessionFactory.getCurrentSession();
+        Query query = session.createQuery(" UPDATE io.khasang.snet.entity.profile.Profile " +
+                "SET avatar = :avatar WHERE login = :userLogin");
+        query.setParameter("avatar",avatarName);
+        query.setParameter("userLogin",userLogin);
+        int result = query.executeUpdate();
     }
 
     @Override
