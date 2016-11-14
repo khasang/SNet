db2fc44fd042b8da60af9d00db9cac766ae8c34c
Not Sure <ASir2089@gmail.com>
2016-11-14 17:06:35 +0300

    Added mappings for searching dialog or creation new one. Also added mapping for remove chat

--- src/main/java/io/khasang/snet/controller/ChatController.java
+++ src/main/java/io/khasang/snet/controller/ChatController.java
@@ -9,8 +9,12 @@ import io.khasang.snet.service.ChatJsonTokenizer;
 import io.khasang.snet.service.MessageTokenizer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.security.Principal;
 import java.util.Collection;
@@ -56,5 +60,27 @@ public class ChatController {
         User sender = new User();
         sender.setLogin(SecurityContextHolder.getContext().getAuthentication().getName());
         return messageTokenizer.addNew(json, sender);
+    }
+
+    @RequestMapping(value = "/newDialog", method = RequestMethod.GET)
+    public ModelAndView newDialog(@RequestParam(value = "login") String login) {
+        User receiver = userDAO.getUserByName(login);
+        User sender = userDAO.getUserByName(SecurityContextHolder.getContext().getAuthentication().getName());
+        Chat chat = chatJsonTokenizer.getExistedDialog(sender, receiver);
+        ModelAndView modelAndView = new ModelAndView("messages");
+        modelAndView.addObject("chat_id", chat.getID());
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/chat/remove", method = RequestMethod.POST)
+    public Object removeOne(@RequestBody String chatId) {
+        User current = userDAO.getUserByName(SecurityContextHolder.getContext().getAuthentication().getName());
+        Chat willRemoved = new Chat();
+        try {
+            willRemoved.setID(Long.parseLong(chatId));
+        } catch (NumberFormatException exc) {
+            return String.format("Wrong parameter: %s", chatId);
+        }
+        return chatJsonTokenizer.deleteExists(willRemoved, current);
     }
 }
