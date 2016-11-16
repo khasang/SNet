d69b136d7b3075286df03dfdd249b943cf06fa17
Not Sure <ASir2089@gmail.com>
2016-11-17 01:32:43 +0300

    Fixed views

--- web/WEB-INF/views/js/chats.js
+++ web/WEB-INF/views/js/chats.js
@@ -1,29 +1,38 @@
-function callChats() {
+function callChats(workingDir) {
+
+    if (workingDir==null) workingDir = '';
+    var service = workingDir + '/chat/all';
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState==4 && this.status==200) {
-            printAllChats(this.responseText);
+            printAllChats(this.responseText,workingDir);
         }
     };
-    xhttp.open("GET", "/chat/all", true);
+    xhttp.open("GET", service, true);
     xhttp.send();
+
 }
 
-function printAllChats(jsonResponse) {
+function printAllChats(jsonResponse,workingDir) {
+
     var chats = JSON.parse(jsonResponse);
     var out = "";
     var i;
     for (i = 0; i < chats.length; i++) {
         out += '<div>Chat: ' + chats[i].description + '<div class="col-sm-4 pull-right">' +
-            '<a href="/messages?id=' + chats[i].id +
+            '<a href="' + workingDir + '/messages?id=' + chats[i].id +
             '"><button class="btn btn-primary btn-xs">Read messages</button></a>' +
-            '<button onclick="removeOne(' + chats[i].id + ')" class="btn btn-danger btn-xs">Remove chat</button>' +
+            '<button onclick="removeOne(' + chats[i].id + ',' + workingDir + ')" class="btn btn-danger btn-xs">Remove chat</button>' +
             '</div></div><hr>';
     }
     document.getElementById("listChats").innerHTML = out;
+
 }
 
-function removeOne(chatId) {
+function removeOne(chatId, workingDir) {
+
+    if (workingDir==null) workingDir = '';
+    var service = workingDir + '/chat/remove';
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
@@ -36,6 +45,7 @@ function removeOne(chatId) {
         }
     };
 
-    xhttp.open("POST","/chat/remove", true);
+    xhttp.open("POST",service, true);
     xhttp.send(chatId);
+
 }
