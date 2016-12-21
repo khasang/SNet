8b9a362f4758c3de6d97e83d82e3340b8a8e751e
Not Sure <ASir2089@gmail.com>
2016-12-21 19:06:22 +0300

    Add method for getting list of test samples

--- src/test/java/io/khasang/snet/repository/messaging/MessagingSuite.java
+++ src/test/java/io/khasang/snet/repository/messaging/MessagingSuite.java
@@ -6,6 +6,9 @@ import io.khasang.snet.repository.AbstractRegistrySearcher;
 import io.khasang.snet.repository.userauth.UserDAO;
 import org.apache.log4j.Logger;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /* Utility class for creating test samples */
 class MessagingSuite {
     // Creates chat's sample with AbstractRegistrySearcher instance
@@ -25,6 +28,15 @@ class MessagingSuite {
         chatUtils.saveChat(chat);
         LOGGER.debug(String.format("Saved new chat entity: [%s]", chat));
         return chat;
+    }
+
+    // Creates list of chat samples
+    static List<Chat> getListSamples(Logger LOGGER, ChatUtils chatUtils, String desc, int amount) {
+        List<Chat> chats = new ArrayList<>();
+        for (int i = 0; i < amount; i++) {
+            chats.add(getSampleChat(LOGGER, chatUtils, String.format("%s #%d",desc, i+1)));
+        }
+        return chats;
     }
 
     // Creates user's sample
